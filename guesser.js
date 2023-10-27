80874e569f1fe0403e264b68149ee9f73a8b82f5
bensimms1 <66845757+bensimms1@users.noreply.github.com>
2023-10-27 17:01:00 -0500

    Add code guesser

---
+++ guesser.js
@@ -0,0 +1,46 @@
+var guess = 0;
+var sleep = 0;
+var display = document.querySelector("div.styles__titleText___2nfVS-camelCase");
+function reactHandler(){return Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode;};
+function generateRandomId() {
+  // Generate a random 7-digit code with leading zeros
+  const randomId = Math.floor(Math.random() * 10000000).toString().padStart(7, '0');
+  return randomId;
+}
+function checkGameExists() {
+  const randomId = generateRandomId();
+  const url = `https://fb.blooket.com/c/firebase/id?id=${randomId}`;
+
+  fetch(url, {
+    method: 'GET',
+    credentials: 'include', // Include cookies
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success === true) {
+        console.log("Game found:", randomId);
+        display.innerHTML = "Game Found!"
+        reactHandler().setState({client:{hostId:randomId.toString()}});
+        reactHandler().onJoin();
+      } else {
+        console.log("No game found for:", randomId);
+        reactHandler().setState({client:{hostId:randomId.toString()}});
+        guess++;
+        display.innerHTML = "Guesses: " + guess;
+        sleep++;
+        if (sleep > 15) {
+          setTimeout(checkGameExists, 1000); // Retry after a delay of 1 seconds
+          sleep = 0;
+        } else {
+          checkGameExists(); // Retry immediately
+        }
+      }
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      // Handle errors here
+    });
+}
+
+// Call checkGameExists to start the process
+checkGameExists();
